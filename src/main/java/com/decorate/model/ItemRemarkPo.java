df2b3da677709707f2ebff87a39197c1cf5ddb29
turningOwei <175408322@qq.com>
2019-01-03 19:49:11 +0800

    mybatis改造

---
+++ src/main/java/com/decorate/model/ItemRemarkPo.java
@@ -0,0 +1,15 @@
+package com.decorate.model;
+
+import lombok.Data;
+
+/**
+ * 描述:
+ *
+ * @author turningOwei
+ * @date 2018-12-25 22:29
+ */
+@Data
+public class ItemRemarkPo extends ItemRemark {
+    private String itemTypeName;
+    private String itemName;
+}
